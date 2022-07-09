e4ffa484dc82dffb40cfd203b19662333b9972f8
ishyshov <ishyshov@workfusion.com>
2022-07-09 20:04:00 +0300

    stage 3

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -5,23 +5,79 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
+    static Map<String, String> map = Map.of("1", "GET", "2", "PUT", "3", "DELETE", "exit", "exit");
 
     public static void main(String[] args) {
         String address = "127.0.0.1";
         int port = 23456;
+
+        String request = getInput();
+        String response = sendRequest(address, port, request);
+        String command = request.split(" ", 2)[0];
+
+        switch (command+response.split(" ", 2)[0]) {
+            case "PUT200":
+                System.out.println("The response says that the file was created!");
+                break;
+            case "PUT403":
+                System.out.println("The response says that creating the file was forbidden!");
+                break;
+            case "GET200":
+                System.out.printf("The content of the file is: %s%s", response.split(" ", 2)[1], System.lineSeparator());
+                break;
+            case "GET404":
+                System.out.println("The response says that the file was not found!");
+                break;
+            case "DELETE200":
+                System.out.println("The response says that the file was successfully deleted!");
+                break;
+            case "DELETE404":
+                System.out.println("The response says that the file was not found!");
+                break;
+            case "exit":
+            default:
+                break;
+        }
+
+    }
+
+    private static String getInput() {
+        StringBuilder request = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter action (1 - get a file, 2 - create a file, 3 - delete a file):");
+        String command = scanner.next();
+
+        request.append(map.get(command)).append(" ");
+        if (!"exit".equals(command)) {
+            System.out.println("Enter filename:");
+            request.append(scanner.next()).append(" ");
+
+            if ("2".equals(command)) {
+                System.out.println("The content of the file is:");
+                request.append(scanner.nextLine()).append(scanner.nextLine());
+            }
+        }
+        return request.toString();
+    }
+
+    private static String sendRequest(String address, int port, String request) {
+        String response = null;
+
         try (
                 Socket socket = new Socket(InetAddress.getByName(address), port);
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
         ) {
-            System.out.println("Client started!");
-            String outputText = "Give me everything you have!";
-            output.writeUTF(outputText);
-            System.out.println("Sent: " + outputText);
-            String inputText = input.readUTF();
-            System.out.println("Received: " + inputText);
+            output.writeUTF(request);
+            response = input.readUTF();
+            System.out.println("Received: " + response);
+
+            return response;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -2,35 +2,100 @@ package server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
 
+    static String address = "127.0.0.1";
+    static int port = 23456;
+
+    private static final String DATA_DIR = System.getProperty("user.dir") + File.separator + "src" + File.separator + "server" + File.separator + "data" + File.separator;
+
     public static void main(String[] args) {
-        String address = "127.0.0.1";
-        int port = 23456;
-        try (
-                ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));
-        ) {
+        String command = "";
+        File path = new File(DATA_DIR);
+        System.out.println(DATA_DIR);
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+
+        try (ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));) {
             System.out.println("Server started!");
-            try (
-                    Socket socket = server.accept();
-                    DataInputStream input = new DataInputStream(socket.getInputStream());
-                    DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-            ) {
-                String inputText = input.readUTF();
-                System.out.println("Received: " + inputText);
-                String outputText = "All files were sent!";
-                output.writeUTF(outputText);
-                System.out.println("Sent: " + outputText);
+            while (!"exit".equals(command)) {
+                try (
+                        Socket socket = server.accept();
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                ) {
+                    String result = "";
+                    String inputText = input.readUTF();
+                    System.out.println("Received: " + inputText);
+                    String[] lines = inputText.split(" ", 3);
+                    command = lines[0];
+
+                    switch (command) {
+                        case "PUT":
+                            result = createFile(lines[1], lines[2]);
+                            output.writeUTF(result);
+                            break;
+                        case "GET":
+                            result = readFile(lines[1]);
+                            output.writeUTF(result);
+                            break;
+                        case "DELETE":
+                            result = deleteFile(lines[1]);
+                            output.writeUTF(result);
+                            break;
+                        case "exit":
+                            output.writeUTF("");
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String deleteFile(String fileName) {
+        try {
+            Files.delete(Path.of(DATA_DIR + fileName));
+            return "200";
+        } catch (IOException e) {
+            return "404";
+        }
+    }
+
+    private static String readFile(String fileName) {
+        try (Scanner sc = new Scanner(new File(DATA_DIR + fileName));
+        ) {
+            return "200 " + sc.nextLine();
+        } catch (FileNotFoundException e) {
+            return "404";
+        }
+    }
+
+    private static String createFile(String fileName, String content) {
+        if (new File(DATA_DIR + fileName).exists()) {
+            return "403";
+        }
+        try (FileWriter file = new FileWriter(DATA_DIR + fileName)) {
+            file.write(content);
+        } catch (IOException e) {
+            return "403";
+        }
+        return "200";
     }
 
 }
